b05b8f8b4a98aa5df66ce8e3f9b81441dcad1f21
Patrick Rodrigues <patrick.pwall@gmail.com>
2018-09-01 17:04:57 -0300

    - Added bootstrap and font-awesome to project - UI module implemented

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpService} from "./services/http.service";
 import {HttpClientModule} from "@angular/common/http";
+import {UiModule} from './ui/ui.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import {HttpClientModule} from "@angular/common/http";
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    UiModule,
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
