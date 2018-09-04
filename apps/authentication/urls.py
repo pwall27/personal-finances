eae679e67f915c34b8f8d6c8603b27a1598e7f58
Patrick Rodrigues <patrick.pwall@gmail.com>
2018-09-03 23:25:12 -0300

    - Changed slash on urls

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import MyTokenObtainPairView, MyTokenRefreshView
 
 urlpatterns = [
-    path('/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
 ]
--- apps/transaction/urls.py
+++ apps/transaction/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import TransactionViewSet
 
 urlpatterns = [
-    path('/', TransactionViewSet.as_view({'get': 'list', 'post': 'create'}), name='transactions'),
+    path('', TransactionViewSet.as_view({'get': 'list', 'post': 'create'}), name='transactions'),
 ]
